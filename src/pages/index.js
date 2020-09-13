ea6ab82d1ea233a5105d6e707a1d775c86d8fbf5
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2020-09-13 19:51:42 -0400

    working with plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link>
-      <h1>Hello Gatsby!</h1>
-      <p>What a world.</p>
-      <img src="http://source.unsplash.com/random/400x200" alt="" />
-    </div>
+    <Layout>
+      <div style={{ color: `purple` }}>
+        <Link to="/contact/">Contact</Link>
+        <h1>Hello Gatsby!</h1>
+        <p>What a world.</p>
+        <img src="http://source.unsplash.com/random/400x200" alt="" />
+      </div>
+    </Layout>
   );
 }
