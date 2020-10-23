eab0f8ddc437406367274614f1c082bc4d684156
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2020-10-22 20:05:02 -0400

    adding graphQL to the soup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 
 export default function Home() {
   return (
     <Layout>
-      <div style={{ color: `purple` }}>
-        <Link to="/contact/">Contact</Link>
-        <h1>Hello Gatsby!</h1>
-        <p>What a world.</p>
-        <img src="http://source.unsplash.com/random/400x200" alt="" />
+      <h1>Amazing Pandas Eating Things</h1>
+      <div>
+        <img
+          src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+          alt="Group of pandas eating bamboo"
+        />
       </div>
     </Layout>
-  );
+  )
 }
