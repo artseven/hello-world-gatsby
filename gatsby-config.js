8777bc02a0b6e448ca948434716c9ec4e23b7bef
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2020-10-22 20:55:35 -0400

    data transformers for GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,19 @@
 
 module.exports = {
   siteMetadata: {
-    title: `Title from siteMetadata`,
+    title: `Pandas Eating Lots`,
   },
   /* Your site config here */
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
